938c253e0304439e50a2bbfe948d336f558f432a
kracken6291 <81332625+kracken6291@users.noreply.github.com>
2021-09-07 17:02:43 -0400

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,16 @@
+class paper{
+    constructor(x,y,r){
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.width=width;
+        this.height=this.height;
+        
+        var options={
+            isStatic: false,
+            restituition:0.3,
+            friction:0,
+            density:1.2
+        }
+    }
+}
